46472f2bf74780f4f42760d6bea64894a42ffa3c
PedroPestana19 <73598020+PedroPestana19@users.noreply.github.com>
2021-09-03 17:41:31 +0100

    update

--- src/main.c
+++ src/main.c
@@ -16,8 +16,8 @@
 /*----I2S----------*/
 #define FINAL_FREQ 1000
 #define FREQUENCY (FINAL_FREQ * 2)
-#define SUPER_SAMPLING 15
-#define SAMPLING_FREQ (FREQUENCY * SUPER_SAMPLING)
+#define MULTI_SAMPLING 15
+#define SAMPLING_FREQ (FREQUENCY * MULTI_SAMPLING)
 #define I2S_PORT I2S_NUM_0
 #define SAMPLE_MASK 0Xfff
 #define I2S_EVENT_QUEUE_ITEM_COUNT 10
@@ -25,7 +25,7 @@
 
 /*----INDICATORS----*/
 #define FREQ_PIN GPIO_NUM_14
-#define LED_PIN GPIO_NUM_2
+#define LED_PIN GPIO_NUM_22
 #define LED_FREQ 500
 
 /*----ADC-----------*/
@@ -34,7 +34,7 @@
 /*---------------------------------VARIABLES-------------------------*/
 QueueHandle_t I2S_Event_Queue = NULL;
 QueueHandle_t Data_Queue = NULL;
-uint16_t samples[SUPER_SAMPLING];
+uint16_t samples[MULTI_SAMPLING];
 bool pin = false;
 bool led = false;
 
@@ -46,7 +46,7 @@ const i2s_config_t i2s_config = {
     .communication_format = I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = DMA_BUF_COUNT,
-    .dma_buf_len = SUPER_SAMPLING,
+    .dma_buf_len = MULTI_SAMPLING,
     .use_apll = false,
     .tx_desc_auto_clear = false,
     .fixed_mclk = 0};
@@ -54,9 +54,10 @@ const i2s_config_t i2s_config = {
 esp_adc_cal_characteristics_t adc_config;
 
 /*---------------------------------FUNCTIONS-------------------------*/
+static void GPIO_Config();
 static void I2S_Reader(void *pvParameters);
 static void Data_Transmiter(void *pvParameters);
-static void setupI2S(void);
+static void I2S_Config(void);
 static int sort_desc(const void *a, const void *b);
 static void esp_error(const char *cause, esp_err_t err);
 static void printSamples(void);
@@ -65,44 +66,24 @@ static uint16_t *sampleFilter(uint16_t *buff, uint8_t size);
 /*---------------------------------SETUP-----------------------------*/
 void app_main()
 {
-    gpio_pad_select_gpio(FREQ_PIN);
-    gpio_set_direction(FREQ_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_level(FREQ_PIN, pdFALSE);
-
-    gpio_pad_select_gpio(LED_PIN);
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_level(LED_PIN, pdFALSE);
+    GPIO_Config();
 
     esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, V_REF, &adc_config);
 
     I2S_Event_Queue = xQueueCreate(I2S_EVENT_QUEUE_ITEM_COUNT, sizeof(i2s_event_t));
     if (I2S_Event_Queue == NULL)
-    {
-        printf("I2S Event queue not initializes!!");
-        while (pdTRUE)
-            ;
-    }
+        esp_error("I2S Event queue not initializes!!", -1);
+
     Data_Queue = xQueueCreate(10, sizeof(uint16_t));
     if (Data_Queue == NULL)
-    {
-        printf("Data queue not initializes!!");
-        while (pdTRUE)
-            ;
-    }
-    setupI2S();
+        esp_error("Data queue not initializes!!", -1);
+
+    I2S_Config();
     if (xTaskCreate(I2S_Reader, "I2S_TASK", 1024 * 2, NULL, 1, NULL) != pdPASS)
-    {
-        printf("I2S_Reader not created!!");
-        while (pdTRUE)
-            ;
-    }
+        esp_error("I2S_Reader not created!!", -1);
 
     if (xTaskCreate(Data_Transmiter, "Date_Processor", 1024 * 2, NULL, 1, NULL) != pdPASS)
-    {
-        printf("I2S_Reader not created!!");
-        while (pdTRUE)
-            ;
-    }
+        esp_error("Data_Transmiter not created!!", -1);
 }
 
 /*---------------------------------MY TASKS--------------------------*/
@@ -119,14 +100,12 @@ void I2S_Reader(void *pvParameters)
                 size_t bytesRead = 0;
                 do
                 {
-                    i2s_read(I2S_PORT, samples + bytesRead, SUPER_SAMPLING * 2, &bytesRead, portMAX_DELAY);
-                } while (bytesRead < SUPER_SAMPLING);
-                if (!xQueueSend(Data_Queue, sampleFilter(samples, SUPER_SAMPLING), pdMS_TO_TICKS(2)))
-                {
-                    printf("Send error");
-                    while (1)
-                        ;
-                }
+                    i2s_read(I2S_PORT, samples + bytesRead, MULTI_SAMPLING * 2, &bytesRead, portMAX_DELAY);
+                } while (bytesRead < MULTI_SAMPLING);
+                
+                if (!xQueueSend(Data_Queue, sampleFilter(samples, MULTI_SAMPLING), pdMS_TO_TICKS(2)))
+                    esp_error("Send error", -1);
+
                 gpio_set_level(FREQ_PIN, pin ^= pdTRUE);
             }
         }
@@ -143,7 +122,7 @@ void Data_Transmiter(void *pvParameters)
         if (xQueueReceive(Data_Queue, &sample, portMAX_DELAY) == pdTRUE)
         {
             sample &= SAMPLE_MASK;
-            printf("%d\n", esp_adc_cal_raw_to_voltage(sample, &adc_config));
+            printf("%d\n", esp_adc_cal_raw_to_voltage( sample, &adc_config));
             if (ledCount >= LED_FREQ)
             {
                 gpio_set_level(LED_PIN, led ^= pdTRUE);
@@ -155,6 +134,17 @@ void Data_Transmiter(void *pvParameters)
 }
 
 /*---------------------------------MY FUNCS--------------------------*/
+
+static void GPIO_Config(void){
+    gpio_pad_select_gpio(FREQ_PIN);
+    gpio_set_direction(FREQ_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(FREQ_PIN, pdFALSE);
+
+    gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(LED_PIN, pdFALSE);
+}
+
 static void esp_error(const char *cause, esp_err_t err)
 {
     if (err != ESP_OK)
@@ -173,7 +163,7 @@ static uint16_t *sampleFilter(uint16_t *buff, uint8_t size)
 
 static void printSamples(void)
 {
-    for (int i = 0; i < SUPER_SAMPLING; ++i)
+    for (int i = 0; i < MULTI_SAMPLING; ++i)
     {
         printf("%x ", samples[i]);
     }
@@ -185,10 +175,10 @@ static int sort_desc(const void *a, const void *b)
     return *((uint16_t *)b) - *((uint16_t *)a);
 }
 
-static void setupI2S(void)
+static void I2S_Config(void)
 {
     printf("Sample Rate = %d\nBits per Sample = %d\nNumber DMA Buffers = %d\nSize DMA per Buffer = %d\n",
-           SAMPLING_FREQ, I2S_BITS_PER_SAMPLE_16BIT, DMA_BUF_COUNT, SUPER_SAMPLING);
+           SAMPLING_FREQ, I2S_BITS_PER_SAMPLE_16BIT, DMA_BUF_COUNT, MULTI_SAMPLING);
     esp_err_t err;
     // The I2S config as per the example
 
